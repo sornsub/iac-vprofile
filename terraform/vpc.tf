85a4dd32c53af90a05b25d8ca787d02ffe00122a
sornsub <150174671+sornsub@users.noreply.github.com>
2024-07-11 14:36:51 +0700

    test flow 9

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,11 +1,12 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.2"
+  version = "5.1.2"
 
   name = "terraops-eks"
-  cidr = "172.20.0.0/16"
 
+  cidr = "172.20.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
   private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
   public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
 
